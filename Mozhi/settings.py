e5636f18d98b7c0a1ce19b520f3e60adcd78587a
Appoox <gadha1998@gmail.com>
2026-02-24 17:03:10 +0530

    Removed csrf middelware

--- Mozhi/settings.py
+++ Mozhi/settings.py
@@ -43,10 +43,10 @@ DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://dataset-label.vconsol.com/',
-    'http://localhost:8000',
-    ]
+# CSRF_TRUSTED_ORIGINS = [
+#     'https://dataset-label.vconsol.com',
+#     'http://localhost:8000',
+#     ]
 
 
 # Application definition
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
